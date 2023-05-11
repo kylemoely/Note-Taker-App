9d172ab560b4778296e0a8e67fe3cf5b9ad4f9ff
kylemoely <kylemoely@gmail.com>
2023-05-11 13:55:16 -0500

    Deleted getNotes file and function. Wrote functional api post route for saving a new note to db.json

--- Develop/helpers/getNotes.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-module.exports = () => {
-    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-        err ? console.error(err) : console.log(JSON.parse(data));
-    })
-}
--- Develop/server.js
+++ Develop/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const uuid = require('./helpers/uuid');
 const getNotes = require('./helpers/getNotes');
-
+const fs = require('fs');
 
 const PORT = 3001;
+
 
 const app = express();
 
@@ -20,8 +21,19 @@ app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/db/db.json'));
 });
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', async (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuid();
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if(err){
+            console.error(err);
+        }
+        const notes = JSON.parse(data);
+        notes.push(newNote);
 
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => err ? console.error(err) : console.log("Note saved!"));
+
+    })
 });
 
 app.get('/*', (req, res) => {
