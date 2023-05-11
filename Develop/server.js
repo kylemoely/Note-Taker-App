1694bf0786781b566f0120f8a9fae52514afdfe0
kylemoely <kylemoely@gmail.com>
2023-05-10 22:43:51 -0500

    Created server.js file and set up express server

---
+++ Develop/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
